eaf319ddbd18fef9a5e7456f314d6b93d44db82c
Henning Andersen <henning.andersen@elastic.co>
2020-04-14 12:02:43 +0200

    Test state simulation

--- x-pack/plugin/autoscaling/src/test/java/org/elasticsearch/xpack/autoscaling/storage/ReactiveStorageDeciderTests.java
+++ x-pack/plugin/autoscaling/src/test/java/org/elasticsearch/xpack/autoscaling/storage/ReactiveStorageDeciderTests.java
@@ -6,6 +6,7 @@
 
 package org.elasticsearch.xpack.autoscaling.storage;
 
+import com.carrotsearch.hppc.ObjectLookupContainer;
 import org.elasticsearch.Version;
 import org.elasticsearch.cluster.ClusterInfo;
 import org.elasticsearch.cluster.ClusterModule;
@@ -17,6 +18,7 @@ import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.node.DiscoveryNodeRole;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.cluster.routing.RoutingNode;
 import org.elasticsearch.cluster.routing.RoutingNodes;
 import org.elasticsearch.cluster.routing.RoutingTable;
 import org.elasticsearch.cluster.routing.ShardRouting;
@@ -24,6 +26,7 @@ import org.elasticsearch.cluster.routing.ShardRoutingState;
 import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
 import org.elasticsearch.cluster.routing.allocation.allocator.BalancedShardsAllocator;
 import org.elasticsearch.cluster.routing.allocation.allocator.ShardsAllocator;
+import org.elasticsearch.cluster.routing.allocation.decider.AllocationDecider;
 import org.elasticsearch.cluster.routing.allocation.decider.AllocationDeciders;
 import org.elasticsearch.cluster.routing.allocation.decider.AwarenessAllocationDecider;
 import org.elasticsearch.cluster.routing.allocation.decider.Decision;
@@ -33,6 +36,7 @@ import org.elasticsearch.cluster.routing.allocation.decider.SameShardAllocationD
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
+import org.elasticsearch.common.collect.Tuple;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.ESTestCase;
@@ -44,6 +48,7 @@ import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -51,11 +56,14 @@ import java.util.Set;
 import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import static org.hamcrest.Matchers.describedAs;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
@@ -64,7 +72,8 @@ import static org.mockito.Mockito.when;
 
 public class ReactiveStorageDeciderTests extends ESTestCase {
 
-    public static final List<String> SOME_ALLOCATION_DECIDERS = Arrays.asList(SameShardAllocationDecider.NAME, AwarenessAllocationDecider.NAME, EnableAllocationDecider.NAME);
+    private static final List<String> SOME_ALLOCATION_DECIDERS = Arrays.asList(SameShardAllocationDecider.NAME,
+        AwarenessAllocationDecider.NAME, EnableAllocationDecider.NAME);
 
     public void testNoTierSpecified() {
         String attribute = randomBoolean() ? null : randomAlphaOfLength(8);
@@ -112,11 +121,11 @@ public class ReactiveStorageDeciderTests extends ESTestCase {
 
         assertThat(ReactiveStorageDecider.isDiskOnlyNoDecision(decision), is(false));
     }
+
     public void testScale() {
-        // todo: ensure this works as long as we have at least 2 data nodes, but only one hot node.
-        // so far, we need at least 2 hot nodes, reactive storage decider cannot handle just one node yet.
         int hotNodes = randomIntBetween(1, 8);
-        int warmNodes = randomIntBetween(1, 3);
+        // todo: work around DiskThresholdDecider single node early termination (no watermarks observed).
+        int warmNodes = randomIntBetween(hotNodes == 1 ? 1 : 0, 3);
         ClusterState state = ClusterState.builder(new ClusterName("test")).build();
 
         // -1 to ensure we have one less shard copy than #nodes - since otherwise a copy may not be able to be allocated anywhere
@@ -177,6 +186,73 @@ public class ReactiveStorageDeciderTests extends ESTestCase {
         verifyDecision(extraNodeState, overAllocatedWithEmptyExtra, decider, contextFactory, AutoscalingDecisionType.NO_SCALE);
     }
 
+    public void testSimulateStartAndAllocate() {
+        int hotNodes = randomIntBetween(1, 8);
+        int warmNodes = randomIntBetween(hotNodes == 1 ? 1 : 0, 3);
+        ClusterState state = ClusterState.builder(new ClusterName("test")).build();
+
+        state = addRandomIndices("initial", hotNodes, hotNodes - 1, state);
+
+        state = addDataNodes("hot", "hot", state, hotNodes);
+        state = addDataNodes("warm", "warm", state, warmNodes);
+
+        BalancedShardsAllocator allocator = new BalancedShardsAllocator(Settings.EMPTY);
+        Collection<AllocationDecider> systemAllocationDeciders = ClusterModule.createAllocationDeciders(Settings.EMPTY,
+            new ClusterSettings(Settings.EMPTY, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS), Collections.emptyList());
+        AllocationDecider mockDecider = new AllocationDecider() {
+            @Override
+            public Decision canAllocate(ShardRouting shardRouting, RoutingNode node, RoutingAllocation allocation) {
+                String nodeName = node.node().getName();
+                String nodeDigit = nodeName.substring(nodeName.length()-1);
+                return (Integer.parseInt(nodeDigit) & 1) == (shardRouting.shardId().id() & 1) ? Decision.YES : Decision.NO;
+            }
+
+            @Override
+            public Decision canRemain(ShardRouting shardRouting, RoutingNode node, RoutingAllocation allocation) {
+                return canAllocate(shardRouting, node, allocation);
+            }
+        };
+
+        AllocationDeciders allocationDeciders =
+            new AllocationDeciders(Stream.concat(Stream.of(mockDecider), systemAllocationDeciders.stream()).collect(Collectors.toList()));
+        ClusterInfo underAllocated = createClusterInfo(state, 1L, 0L);
+        TestAutoscalingDeciderContext context = new TestAutoscalingDeciderContext(state, underAllocated, allocator, allocationDeciders);
+
+        ClusterState lastState = null;
+        int maxRounds = state.getRoutingNodes().unassigned().size() + 1;
+        int count = 0;
+        while (lastState != state && count++ < maxRounds) {
+            ClusterState simulatedState = ReactiveStorageDecider.simulateAllocationOfState(state, context);
+            assertThat(simulatedState.nodes(), sameInstance(state.nodes()));
+            assertThat(simulatedState.metadata().indices().keys().toArray(), equalTo(state.metadata().indices().keys().toArray()));
+            assertThat(simulatedState.getRoutingNodes().shardsWithState(ShardRoutingState.INITIALIZING), empty());
+            assertThat(simulatedState.getRoutingNodes().shardsWithState(ShardRoutingState.RELOCATING), empty());
+            RoutingAllocation allocation = new RoutingAllocation(allocationDeciders, simulatedState.getRoutingNodes(), simulatedState,
+                underAllocated,
+                System.nanoTime());
+            StreamSupport.stream(simulatedState.getRoutingNodes().spliterator(), false)
+                .flatMap(n -> n.shardsWithState(ShardRoutingState.STARTED).stream().map(s -> Tuple.tuple(n, s)))
+                .forEach(t -> validateAllocation(t.v1(), t.v2(), allocation));
+            StreamSupport.stream(simulatedState.getRoutingNodes().unassigned().spliterator(), false)
+                .forEach(s -> validateUnassigned(s, allocation));
+
+            lastState = state;
+            state = startRandomShards(state, context);
+        }
+        assertThat(state, sameInstance(lastState));
+        assertThat(ReactiveStorageDecider.simulateAllocationOfState(state, context), sameInstance(state));
+    }
+
+    private void validateUnassigned(ShardRouting unassigned, RoutingAllocation allocation) {
+        for (RoutingNode routingNode : allocation.routingNodes()) {
+            assertThat(allocation.deciders().canAllocate(unassigned, routingNode, allocation).type(), not(equalTo(Decision.Type.YES)));
+        }
+    }
+
+    private void validateAllocation(RoutingNode node, ShardRouting shard, RoutingAllocation allocation) {
+        assertThat(allocation.deciders().canRemain(shard, node, allocation).type(), equalTo(Decision.Type.YES));
+    }
+
     private ClusterState startRandomShards(ClusterState state, TestAutoscalingDeciderContext context) {
         RoutingNodes routingNodes = new RoutingNodes(state, false);
         RoutingAllocation allocation = new RoutingAllocation(
@@ -196,14 +272,27 @@ public class ReactiveStorageDeciderTests extends ESTestCase {
             .forEach(s -> {
                 allocation.routingNodes().startShard(logger, s, allocation.changes());
             });
-
         shards.stream()
             .filter(s -> s.primary() == true)
             .forEach(s -> {
                 allocation.routingNodes().startShard(logger, s, allocation.changes());
             });
-
         context.shardsAllocator().allocate(allocation);
+
+        // ensure progress by only relocating a shard if we started more than one shard.
+        if (shards.size() > 1 && randomBoolean()) {
+            List<ShardRouting> started = allocation.routingNodes().shardsWithState(ShardRoutingState.STARTED);
+            if (started.isEmpty() == false) {
+                ShardRouting toMove = randomFrom(started);
+                Set<RoutingNode> candidates = StreamSupport.stream(routingNodes.spliterator(), false)
+                    .filter(n -> allocation.deciders().canAllocate(toMove, n, allocation) == Decision.YES)
+                    .collect(Collectors.toSet());
+                if (candidates.isEmpty() == false) {
+                    allocation.routingNodes().relocateShard(toMove, randomFrom(candidates).nodeId(), 0L, allocation.changes());
+                }
+            }
+        }
+
         return ReactiveStorageDecider.updateClusterState(state, allocation);
 
     }
@@ -309,11 +398,47 @@ public class ReactiveStorageDeciderTests extends ESTestCase {
         return ClusterState.builder(state).nodes(builder).build();
     }
 
-    private DiscoveryNode newDataNode(String tier, String nodeName) {
+    private static DiscoveryNode newDataNode(String tier, String nodeName) {
         return new DiscoveryNode(nodeName, UUIDs.randomBase64UUID(), buildNewFakeTransportAddress(),
             Map.of("tier", tier), Set.of(DiscoveryNodeRole.DATA_ROLE), Version.CURRENT);
     }
 
+    private static enum NodeSet {
+        hot("hot"),
+        warm("warm"),
+        extra("hot");
+
+        private final String tier;
+
+        NodeSet(String tier) {
+            this.tier = tier;
+        }
+
+        ClusterState addDataNodes(ClusterState state, int nodes) {
+            String prefix = name();
+            DiscoveryNodes.Builder builder = DiscoveryNodes.builder(state.nodes());
+            IntStream.range(0, nodes).mapToObj(i -> newDataNode(tier, prefix + "_" + i)).forEach(builder::add);
+            return ClusterState.builder(state).nodes(builder).build();
+        }
+
+        static NodeSet fromNode(DiscoveryNode node) {
+            return valueOf(node.getName().substring(0, node.getName().indexOf("_")));
+        }
+    }
+
+    private static class ClusterInfoSpec {
+        final long free;
+        final long total;
+
+        public ClusterInfoSpec(long free, long total) {
+            this.free = free;
+            this.total = total;
+        }
+    }
+
+    private ClusterInfoSpec clusterInfo(long free, long total) {
+        return new ClusterInfoSpec(free, total);
+    }
     Matcher<AutoscalingDecision> decisionType(AutoscalingDecisionType type, ClusterState state) {
         return new TypeSafeMatcher<AutoscalingDecision>() {
             @Override
