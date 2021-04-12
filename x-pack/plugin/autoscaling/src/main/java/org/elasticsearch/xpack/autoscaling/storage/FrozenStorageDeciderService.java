1256d2b53b5c3c789b0c4e90148a01039e673302
Henning Andersen <henning.andersen@elastic.co>
2021-04-12 13:44:09 +0200

    Frozen decider

--- x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/storage/FrozenStorageDeciderService.java
+++ x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/storage/FrozenStorageDeciderService.java
@@ -7,24 +7,32 @@
 
 package org.elasticsearch.xpack.autoscaling.storage;
 
+import org.elasticsearch.cluster.ClusterInfo;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.node.DiscoveryNodeRole;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.xpack.autoscaling.capacity.AutoscalingCapacity;
 import org.elasticsearch.xpack.autoscaling.capacity.AutoscalingDeciderContext;
 import org.elasticsearch.xpack.autoscaling.capacity.AutoscalingDeciderResult;
 import org.elasticsearch.xpack.autoscaling.capacity.AutoscalingDeciderService;
+import org.elasticsearch.xpack.cluster.routing.allocation.DataTierAllocationDecider;
+import org.elasticsearch.xpack.core.DataTier;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
 
 public class FrozenStorageDeciderService implements AutoscalingDeciderService {
     public static final String NAME = "frozen_storage";
 
     public static final Setting<Double> PERCENTAGE = Setting.doubleSetting("percentage", 5.0d, 0.0d);
-
 
     @Override
     public String name() {
@@ -33,7 +41,34 @@ public class FrozenStorageDeciderService implements AutoscalingDeciderService {
 
     @Override
     public AutoscalingDeciderResult scale(Settings configuration, AutoscalingDeciderContext context) {
-        return null;
+        Metadata metadata = context.state().metadata();
+        long dataSetSize = StreamSupport.stream(metadata.spliterator(), false)
+            .filter(imd -> isFrozenIndex(imd.getSettings()))
+            .mapToLong(imd -> estimateSize(imd, context.info()))
+            .sum();
+
+        long storageSize = (long) (PERCENTAGE.get(configuration) * dataSetSize);
+        return new AutoscalingDeciderResult(AutoscalingCapacity.builder().total(storageSize, null).build(), new FrozenReason(dataSetSize));
+    }
+
+    private long estimateSize(IndexMetadata imd, ClusterInfo info) {
+        int copies = imd.getNumberOfReplicas() + 1;
+        return IntStream.range(0, imd.getNumberOfShards())
+            .mapToObj(s -> new ShardId(imd.getIndex(), s))
+            .mapToLong(s -> info.getShardDataSetSize(s).orElse(0L))
+            .map(s -> s * copies)
+            .sum();
+    }
+
+    static boolean isFrozenIndex(Settings indexSettings) {
+        String tierPreference = DataTierAllocationDecider.INDEX_ROUTING_PREFER_SETTING.get(indexSettings);
+        String[] preferredTiers = DataTierAllocationDecider.parseTierList(tierPreference);
+        if (preferredTiers.length >= 1 && preferredTiers[0].equals(DataTier.DATA_FROZEN)) {
+            assert preferredTiers.length == 1 : "frozen tier preference must be frozen only";
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
@@ -47,11 +82,19 @@ public class FrozenStorageDeciderService implements AutoscalingDeciderService {
     }
 
     public static class FrozenReason implements AutoscalingDeciderResult.Reason {
-        public FrozenReason(StreamInput in) {
+        private final long totalDataSetSize;
 
+        public FrozenReason(long totalDataSetSize) {
+            this.totalDataSetSize = totalDataSetSize;
         }
+
+        public FrozenReason(StreamInput in) throws IOException {
+            this.totalDataSetSize = in.readLong();
+        }
+
         @Override
         public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+            builder.field("total_data_set_size", totalDataSetSize);
             return builder;
         }
 
@@ -62,12 +105,12 @@ public class FrozenStorageDeciderService implements AutoscalingDeciderService {
 
         @Override
         public void writeTo(StreamOutput out) throws IOException {
-
+            out.writeLong(totalDataSetSize);
         }
 
         @Override
         public String summary() {
-            return null;
+            return "total data set size [" + totalDataSetSize + "]";
         }
     }
 }
