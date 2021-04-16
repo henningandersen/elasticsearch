3c56bd3f396ad66fca37f498e3ed01058b623fdb
Henning Andersen <henning.andersen@elastic.co>
2021-04-16 19:00:54 +0200

    Support logging (remove from PR)

--- buildSrc/src/main/java/org/elasticsearch/gradle/testclusters/ElasticsearchNode.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/testclusters/ElasticsearchNode.java
@@ -1221,6 +1221,9 @@ public class ElasticsearchNode implements TestClusterConfiguration {
 
         baseConfig.put("action.destructive_requires_name", "false");
 
+        if (System.getProperty("tests.es.logger.level") != null) {
+            baseConfig.put("logger.level", System.getProperty("tests.es.logger.level"));
+        }
         HashSet<String> overriden = new HashSet<>(baseConfig.keySet());
         overriden.retainAll(settings.keySet());
         overriden.removeAll(OVERRIDABLE_SETTINGS);
